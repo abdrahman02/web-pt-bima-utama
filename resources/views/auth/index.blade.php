a1c1831bfb80a6b08a3979d76c109301370487fe
abdrahman02 <abdul.190180078@mhs.unimal.ac.id>
2023-09-08 23:51:51 +0700

    fitur login

---
+++ resources/views/auth/index.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    {{--
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/Logo-Tut-Wuri-Handayani.png') }}"> --}}
+    {{--
+    <link rel="icon" type="image/png" href="{{ asset('img/Logo-Tut-Wuri-Handayani.png') }}"> --}}
+    <title>
+        PT. BIMA UTAMA
+    </title>
+    <!-- Nucleo Icons -->
+    <link href="{{ asset('auth/css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('auth/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="{{ asset('auth/js/kitfontawesome.js') }}"></script>
+    <link href="{{ asset('auth/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link id="pagestyle" href="{{ asset('auth/css/argon-dashboard.css') }}" rel="stylesheet" />
+</head>
+
+<body>
+    <main class="main-content mt-0">
+        <section>
+            <div class="page-header min-vh-100">
+                <div class="container">
+                    <div class="col-4 shadow-lg mx-auto">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="col-12 d-flex flex-column mx-lg-0 mx-auto text-center">
+                                    <h4 class="font-weight-bolder">
+                                        LOGIN
+                                    </h4>
+                                    <div class="pb-0 text-start mx-auto">
+                                        <img src="backend/assets/img/Logo-Tut-Wuri-Handayani.png" alt="logo"
+                                            style="height: 100px;">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <form action="{{ route('authenticate') }}" method="post">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <input type="text" class="form-control form-control-lg" name="username"
+                                            placeholder="Username" required autofocus />
+                                    </div>
+                                    <div class="mb-3">
+                                        <input type="password" class="form-control form-control-lg" name="password"
+                                            placeholder="Password" aria-label="Password" required />
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">
+                                            Login
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!--   Core JS Files   -->
+    <script src="{{ asset('backend/assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/core/bootstrap.min.js') }}"></script>
+    <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="{{ asset('backend/assets/js/argon-dashboard.min.js') }}"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'index']);
+Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
